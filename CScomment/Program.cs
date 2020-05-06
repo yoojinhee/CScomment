3d449b30db7c103ebff5999bdc43c2ca78a9edfc
yoojinhee <52348335+yhujinhui@users.noreply.github.com>
2020-05-06 15:31:12 +0900

    초기 커밋.

---
+++ CScomment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CScomment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.Write("x :");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("y :");
+            y = int.Parse(Console.ReadLine());
+            if (x > 4)
+            {
+                if (y > 2)
+                {
+                    Console.WriteLine(x * y);
+                }
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
